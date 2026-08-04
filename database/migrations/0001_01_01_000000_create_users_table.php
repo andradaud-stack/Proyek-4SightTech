ff54463bb7ecf5926d8eac41d9644c04379838af
Ervina <sheylervina@gmail.com>
2026-08-04 08:30:53 +0700

    mengubah migrasi user tabel

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('id', 36)->primary();
+            $table->id();
+
             $table->string('name');
-            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('identitas', 30)->nullable();
+            $table->string('role')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
